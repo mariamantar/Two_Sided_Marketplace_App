c142c6f0c2252fe7d78022eb4ecf80ae37afa8c9
Mariam <mariam.antar@hotmail.com>
2018-06-25 15:55:40 +1000

    sign up users from UI

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.4.1'
 #user password
 gem 'bcrypt', '~> 3.1.7'
+#creating clean forms
+gem 'simple_form'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_052400) do
+ActiveRecord::Schema.define(version: 2018_06_25_025103) do
 
   create_table "buyer_products", force: :cascade do |t|
     t.integer "Buyer_id"
@@ -65,9 +65,9 @@ ActiveRecord::Schema.define(version: 2018_06_20_052400) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
